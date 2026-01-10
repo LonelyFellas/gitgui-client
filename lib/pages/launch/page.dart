e3d8b9f604f80e1883b04d9b3fe6bef26ab99a6d
darwish <289582304@qq.com>
2026-01-11 00:20:35 +0800

    feat: 增加头部

--- lib/pages/launch/page.dart
+++ lib/pages/launch/page.dart
@@ -9,9 +9,12 @@ class LaunchPage extends StatefulWidget {
   State<LaunchPage> createState() => _LaunchPageState();
 }
 
+enum _RepositoryType { local, remote }
+
 class _LaunchPageState extends State<LaunchPage> {
   late LaunchController _controller;
   final bool _isInstalledGit = Git.instance.isInstalled;
+  _RepositoryType _repositoryType = _RepositoryType.local;
 
   @override
   void initState() {
@@ -27,10 +30,85 @@ class _LaunchPageState extends State<LaunchPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Git Guilar')),
-      body: Center(
-        child: Text(
-          'Git is ${_isInstalledGit ? 'installed' : 'not installed'}',
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {},
+      ),
+      body: Column(
+        children: [
+          Container(
+            color: Colors.grey[200],
+            height: 48,
+            padding: const EdgeInsets.symmetric(
+              horizontal: 12.0,
+              vertical: 6.0,
+            ),
+            child: Row(
+              spacing: 12,
+              children: [
+                ToggleButtons(
+                  isSelected: [
+                    _repositoryType == _RepositoryType.local,
+                    _repositoryType == _RepositoryType.remote,
+                  ],
+                  onPressed: (index) {
+                    setState(() {
+                      _repositoryType = index == 0
+                          ? _RepositoryType.local
+                          : _RepositoryType.remote;
+                    });
+                  },
+                  children: [
+                    _RepositoryTypeButton(
+                      repositoryType: _RepositoryType.local,
+                      isSelected: _repositoryType == _RepositoryType.local,
+                    ),
+                    _RepositoryTypeButton(
+                      repositoryType: _RepositoryType.remote,
+                      isSelected: _repositoryType == _RepositoryType.remote,
+                    ),
+                  ],
+                ),
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: "搜索仓库",
+                      contentPadding: .zero,
+                      filled: true,
+                      hoverColor: Colors.transparent,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(),
+                      prefixIcon: const Icon(Icons.search),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _RepositoryTypeButton extends StatelessWidget {
+  final _RepositoryType repositoryType;
+  final bool isSelected;
+
+  const _RepositoryTypeButton({
+    required this.repositoryType,
+    this.isSelected = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Text(
+        repositoryType == _RepositoryType.local ? "本地仓库" : "远程仓库",
+        style: TextStyle(
+          fontSize: 14,
+          fontWeight: isSelected ? FontWeight.w500 : FontWeight.normal,
         ),
       ),
     );
